6022e8415629cd126038ec570bfe3f02abbb5836
Charles <xxxx>
2026-02-21 15:26:37 +0800

    fix(tray): make TrayController singleton to keep click handler

--- ui_flutter/template/lib/utils/tray_controller.dart
+++ ui_flutter/template/lib/utils/tray_controller.dart
@@ -1,7 +1,14 @@
 import "tray_controller_stub.dart" if (dart.library.io) "tray_controller_io.dart";
 
 abstract class TrayController {
-  static TrayController get instance => getTrayController();
+  static TrayController? _instance;
+
+  /// Singleton.
+  ///
+  /// Do NOT create multiple tray controller instances: on Windows the underlying
+  /// `system_tray` plugin installs a global method-channel callback, so a later
+  /// instance would overwrite the click handler and make the tray icon "dead".
+  static TrayController get instance => _instance ??= getTrayController();
 
   bool get isAvailable;
 
